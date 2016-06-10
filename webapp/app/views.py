7a155a66622032656194876ca58d8db2a5404ad3
Ahmed AlTaha <ahmedsalam.2008@gmail.com>
2016-06-10 04:29:14 +0000

    Simple webserver and API

---
+++ webapp/app/views.py
@@ -0,0 +1,38 @@
+from flask import jsonify, render_template
+
+from app import app
+from cassandra.cluster import Cluster
+from elasticsearch import Elasticsearch
+
+# setting up connections to cassandra
+es = Elasticsearch(['ec2-52-40-68-150.us-west-2.compute.amazonaws.com'])
+cluster = Cluster(['ec2-52-41-40-156.us-west-2.compute.amazonaws.com'])
+session = cluster.connect('payments')
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Kafka'}
+    return render_template("index.html", title='Home', user=user)
+
+@app.route('/api/message/')
+def get_messages():
+    statement = 'SELECT * FROM test'
+    response = session.execute(statement)
+    json_response = [{'id': x.id, 'message': x.message} for x in response]
+    return jsonify(messages=json_response)
+
+@app.route('/api/message/<id>')
+def get_message(id):
+    statement = 'SELECT * FROM test WHERE id={}'.format(id)
+    response = session.execute(statement)
+    json_response = [{'id': x.id, 'message': x.message} for x in response]
+    return jsonify(messages=json_response)
+
+@app.route('/api/payments/<keywords>')
+def get_elastic_search_messages(keywords):
+    result = es.search(
+        index='spark',
+        body={'query': {'match': {'message': keywords}}},
+    )
+    return jsonify(transactions=result['hits']['hits'])
