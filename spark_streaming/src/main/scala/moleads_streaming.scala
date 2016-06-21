a70d0253aeab7c5bcb6f891f84e186600a431dca
Ahmed AlTaha <ahmedsalam.2008@gmail.com>
2016-06-21 06:38:36 +0000

    Minor Spark code cleanups and optimizations
    - Was hoping to reduce processing time but made no gains

--- spark_streaming/src/main/scala/moleads_streaming.scala
+++ spark_streaming/src/main/scala/moleads_streaming.scala
@@ -5,7 +5,6 @@ import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
-import org.apache.spark.sql._
 
 import org.elasticsearch.spark._
 import com.datastax.spark.connector._
@@ -39,7 +38,7 @@ object MoLeadsStreaming {
         val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet).map(_._2)
 
         // parse the json records
-        val adjacencyRDD = messages.map(
+        val adjacencyDStream = messages.map(
             JsonMethods.parse(_).asInstanceOf[JObject]
         ).map(json => {
             implicit val formats = DefaultFormats
@@ -52,20 +51,21 @@ object MoLeadsStreaming {
             val target_name = (json \\ "target" \ "name").extractOpt[String].getOrElse("")
             ActorTargetAdjacency(id, time, message, actor_id, actor_name, target_id, target_name)
         }).filter(_.isValid)
-        //adjacencyRDD.print(2)
+        //adjacencyDStream.print(2)
 
-        // save to Cassandra and ElasticSearch
-        adjacencyRDD.foreachRDD{rdd =>
-            if (rdd.toLocalIterator.nonEmpty) {
-                rdd.saveToCassandra("moleads","adjacency",
-                    SomeColumns("id", "time", "message", "actor_id", "actor_name", "target_id", "target_name")
-                )
-                rdd.saveToEs("moleads/payment")
-            }
+        // save to Cassandra
+        adjacencyDStream.foreachRDD{rdd =>
+            rdd.saveToCassandra("moleads","adjacency",
+                SomeColumns("id", "time", "message", "actor_id", "actor_name", "target_id", "target_name")
+            )
+        }
+        // save to ElasticSearch
+        adjacencyDStream.foreachRDD{rdd =>
+            rdd.saveToEs("moleads/payment")
         }
 
         // get micro batch word counts and save to Cassandra
-        val words = adjacencyRDD.map(_.message).flatMap(
+        val words = adjacencyDStream.map(_.message).flatMap(
             _.replaceAll("[^a-zA-Z]", " ").split(" ")
         ).map(_.trim).filter(_.length > 2).map(_.toLowerCase)
         val pairs = words.map(word => (word, 1))
@@ -73,7 +73,7 @@ object MoLeadsStreaming {
         wordCounts.foreachRDD{ (rdd, time) =>
             val timeDate = new Date(time.milliseconds)
             rdd.map(
-                wordCountPair => (Seq(wordCountPair).toMap, "seconds", timeDate)
+                wordCountPair => (Map(wordCountPair._1 -> wordCountPair._2), "seconds", timeDate)
             ).saveToCassandra("moleads", "word_counts",
                 SomeColumns("word_count" append, "period", "time")
             )
@@ -92,19 +92,5 @@ case class ActorTargetAdjacency(
 ) {
     def isValid = {
         id != 0L && time != "" && message != "" && actor_id != "" && target_id != ""
-    }
-}
-
-
-/** Lazily instantiated singleton instance of SQLContext */
-object SQLContextSingleton {
-
-    @transient  private var instance: SQLContext = _
-
-    def getInstance(sparkContext: SparkContext): SQLContext = {
-        if (instance == null) {
-            instance = new SQLContext(sparkContext)
-        }
-        instance
     }
 }
